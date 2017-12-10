e63f5a65c4d6a2f59d26cd10e2e6f4544b2bf1ee
Ward Poelmans <wpoely86@gmail.com>
2017-12-10 20:39:01 +0100

    Use title, use iframe options and avoid scrollbars

--- urlpreviewer/urlpreview.js
+++ urlpreviewer/urlpreview.js
@@ -13,6 +13,11 @@
     s.href =  Configuration.previewer + "/featherlight/featherlight.min.css";
     $(useTab).append(s)
 
+    s = document.createElement("style");
+    s.type = "text/css";
+    s.append(".myiframebox { overflow-y: auto; }");
+    $(useTab).append(s)
+
     var extractor_req = $.ajax({
         type: "GET",
         url: jsRoutes.api.Files.getMetadataJsonLD(confId, "ncsa.urlextractor/1.0").url,
@@ -37,11 +42,16 @@
             var preview = Configuration.url;
         }
 
-        $(Configuration.tab).append("<h1 align='center'><a href='" + fullurl + "' target='_blank'>" + fullurl + "</a></h1>");
+        $(Configuration.tab).append("<h1 align='center'><a href='" + fullurl + "' target='_blank'>" + extract_data[0][0]['content']['title'] + "</a></h1>");
 
         $(Configuration.tab).append("<a href='" + fullurl + "' id='mypreviewlink'><img class='fit-in-space' src='" + preview + "'/></a>");
 
-        $('#mypreviewlink').featherlight(fullurl, {type:'iframe', loading: 'Loading ' + fullurl, iframeWidth: $(window).width() * 0.8, iframeHeight: $(window).height() * 0.8});
+        // lets assume if it has any value, we shouldn't iframe it.
+        if(extract_data[0][0]['content'].hasOwnProperty('X-Frame-Options')) {
+            $('#mypreviewlink').attr('target', '_blank');
+        } else {
+            $('#mypreviewlink').featherlight(fullurl, {type:'iframe', variant: 'myiframebox', loading: 'Loading ' + fullurl, iframeWidth: $(window).width() * 0.8, iframeHeight: $(window).height() * 0.8});
+        }
 
     }).fail(function(err){
         console.log("Failed to load all scripts and data for URL previewer: " + err['status'] + " - " + err['statusText']);
